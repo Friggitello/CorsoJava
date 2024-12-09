b6910ec4812b7d5e39d6ed9e3c002217fe9abe16
fudarts <federicofiore01@gmail.com>
2024-12-09 10:50:13 +0100

    a

---
+++ lezione9_12/Mese.java
@@ -0,0 +1,7 @@
+package lezione9_12;
+
+public enum Mese {
+
+    GENNAIO, FEBBRAIO;
+
+}
---
+++ lezione9_12/prova.java
@@ -0,0 +1,9 @@
+package lezione9_12;
+
+public class prova {
+
+    public static void main(String[] args) {
+        Mese mese1 = Mese.GENNAIO;
+    }
+
+}
